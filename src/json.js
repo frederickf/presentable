5d8b4d8d7b40b6f6ebd3361b9e236cfd78fd9ec4
frederickf <ffeibel@hotmail.com>
2014-01-12 23:57:13 -0600

    Updated for impress, fixed impress indexing bug

--- src/json.js
+++ src/json.js
@@ -229,7 +229,10 @@ define('json', [], function() {
     json.frameworks.impressjs = {
         SLIDE_SEARCH_STRING: '.step',
         slideIndex: function(slide, i) {
-            return slide.id;
+            if (slide.id) {
+                return slide.id;
+            }
+            return "step-" + (i + 1);
         }
     };
 
